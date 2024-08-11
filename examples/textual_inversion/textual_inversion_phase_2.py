defb8040d0744489653c57be02081b5871891f24
Sharon Lee <sharonal@stanford.edu>
2024-08-11 16:26:18 -0700

    phase 2

--- examples/textual_inversion/textual_inversion_phase_2.py
+++ examples/textual_inversion/textual_inversion_phase_2.py
@@ -842,7 +842,7 @@ def main():
         if args.resume_from_checkpoint != "latest":
             path = os.path.basename(args.resume_from_checkpoint)
         else:
-            # Get the most recent checkpoint
+            # Get the most recent checkpoint (from pretrained_model_name_or_path, not output_dir)
             dirs = os.listdir(args.pretrained_model_name_or_path)
             dirs = [d for d in dirs if d.startswith("checkpoint")]
             dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))
@@ -856,7 +856,7 @@ def main():
             initial_global_step = 0
         else:
             accelerator.print(f"Resuming from checkpoint {path}")
-            accelerator.load_state(os.path.join(args.output_dir, path))
+            accelerator.load_state(os.path.join(args.pretrained_model_name_or_path, path))
             global_step = int(path.split("-")[1])
 
             initial_global_step = global_step
