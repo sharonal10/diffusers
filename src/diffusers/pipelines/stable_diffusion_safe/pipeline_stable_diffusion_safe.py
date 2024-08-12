34cdb9aff8e12138f34e2deef87b59a93ec51fbd
Sharon Lee <sharonal@stanford.edu>
2024-08-11 19:40:56 -0700

    disable nsfw

--- src/diffusers/pipelines/stable_diffusion_safe/pipeline_stable_diffusion_safe.py
+++ src/diffusers/pipelines/stable_diffusion_safe/pipeline_stable_diffusion_safe.py
@@ -309,26 +309,26 @@ class StableDiffusionPipelineSafe(DiffusionPipeline, StableDiffusionMixin, IPAda
         return prompt_embeds
 
     def run_safety_checker(self, image, device, dtype, enable_safety_guidance):
-        if self.safety_checker is not None:
-            images = image.copy()
-            safety_checker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(device)
-            image, has_nsfw_concept = self.safety_checker(
-                images=image, clip_input=safety_checker_input.pixel_values.to(dtype)
-            )
-            flagged_images = np.zeros((2, *image.shape[1:]))
-            if any(has_nsfw_concept):
-                logger.warning(
-                    "Potential NSFW content was detected in one or more images. A black image will be returned"
-                    " instead."
-                    f"{'You may look at this images in the `unsafe_images` variable of the output at your own discretion.' if enable_safety_guidance else 'Try again with a different prompt and/or seed.'}"
-                )
-                for idx, has_nsfw_concept in enumerate(has_nsfw_concept):
-                    if has_nsfw_concept:
-                        flagged_images[idx] = images[idx]
-                        image[idx] = np.zeros(image[idx].shape)  # black image
-        else:
-            has_nsfw_concept = None
-            flagged_images = None
+        # if self.safety_checker is not None:
+        #     images = image.copy()
+        #     safety_checker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(device)
+        #     image, has_nsfw_concept = self.safety_checker(
+        #         images=image, clip_input=safety_checker_input.pixel_values.to(dtype)
+        #     )
+        #     flagged_images = np.zeros((2, *image.shape[1:]))
+        #     if any(has_nsfw_concept):
+        #         logger.warning(
+        #             "Potential NSFW content was detected in one or more images. A black image will be returned"
+        #             " instead."
+        #             f"{'You may look at this images in the `unsafe_images` variable of the output at your own discretion.' if enable_safety_guidance else 'Try again with a different prompt and/or seed.'}"
+        #         )
+        #         for idx, has_nsfw_concept in enumerate(has_nsfw_concept):
+        #             if has_nsfw_concept:
+        #                 flagged_images[idx] = images[idx]
+        #                 image[idx] = np.zeros(image[idx].shape)  # black image
+        # else:
+        has_nsfw_concept = None
+        flagged_images = None
         return image, has_nsfw_concept, flagged_images
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.decode_latents
